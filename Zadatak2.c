adc7f098a0d5c992c90d79d157e91221cc4ec5a5
martabatinic <56558228+martabatinic@users.noreply.github.com>
2020-10-30 23:40:05 +0100

    Update Zadatak2.c

--- Zadatak2.c
+++ Zadatak2.c
@@ -1,10 +1,12 @@
 #define CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 1000
 
-/*. Definirati strukturu osoba (ime, prezime, godina rođenja) i napisati program koji:
+/*2. Definirati strukturu osoba (ime, prezime, godina rođenja) i napisati program koji:
 a) dinamički dodaje novi element na početak liste,
 b) ispisuje listu,
 c) dinamički dodaje novi element na kraj liste,
@@ -12,20 +14,19 @@ d) pronalazi element u listi (po prezimenu),
 e) briše određeni element iz liste,
 U zadatku se ne smiju koristiti globalne varijable.*/
 
-struct _osoba;
-typedef struct _osoba *Pozicija;
 typedef struct _osoba {
 	char ime[MAX];
 	char prezime[MAX];
 	int godinaRodenja;
-	Pozicija *next;
+	struct _osoba *next;
 }Osoba;
 
-Pozicija StvoriOsobu(char *ime, char *prezime, int *godinaRodenja);
-int UnosNaPocetak(Osoba *element, Pozicija P);
-int IspisListe(Pozicija P);
-int UnosNaKraj(Osoba *element, Pozicija P);
-int TraziEelement(char *element, Pozicija P);
+Osoba *UnosPodataka(void);
+int UnosPocetak(Osoba *El, Osoba *Head);
+int IspisListe(Osoba *Head);
+int UnosKraj(Osoba *El, Osoba *Head);
+Osoba *TraziElement(char *prezime, Osoba *Head);
+int BrisiElement(Osoba *El, Osoba *Head);
 
 int main(void)
 {
@@ -34,122 +35,107 @@ int main(void)
 	if (!Head)
 		return -1;
 	Head->next = NULL;
-	Pozicija *what;
-	Osoba *element = NULL;
 
-	int opcija = 0;
-	int n = 0;
-	char ime[MAX] = { 0 };
-	char prezime[MAX] = { 0 };
-	int godinaRodenja = 0;
+	int status = 0;
+	do {
+		int opcija = 0;
+		Osoba *El = NULL;
+		char prezime[MAX] = { 0 };
 
-	printf("Koliko osoba zelite unijeti?\n");
-	scanf("%d", &n);
+		printf("Odaberite zeljenu opciju: \n");
+		printf("1. Dodati novi element na pocetak liste\n");
+		printf("2. Ispisati listu\n");
+		printf("3. Dodati novi element na kraj liste\n");
+		printf("4. Pronaci element u listi (po prezimenu)\n");
+		printf("5. Brisati odredeni element\n");
+		printf("0. ZATVORI PROGRAM\n");
+		scanf("%d", &opcija);
 
-	for(int i=0;i<n;i++){
-		printf("Unesite ime: \n");
-		scanf("%s", ime);
-		printf("Unesite prezime: \n");
-		scanf("%s", prezime);
-		printf("Unesite godinu rodenja: \n");
-		scanf("%d", &godinaRodenja);
-
-		what = StvoriOsobu(ime, prezime, godinaRodenja);
-	} 
-
-	printf("Odaberite opciju: \n");
-	printf("1. Dodavanje elementa na pocetak liste \n");
-	printf("2. Ispis liste \n");
-	printf("3. Dodavanje elementa na kraj liste \n");
-	printf("4. Trazi element u listi (po prezimenu \n");
-	printf("5. Brisanje elementa iz liste \n");
-
-	scanf("%d", &opcija);
-
-	switch (opcija)
-	{
+		switch (opcija)
+		{
 		case(1):
-			UnosNaPocetak(what, Head);
+			UnosPocetak(UnosPodataka(), Head);
 			break;
 		case(2):
 			IspisListe(Head);
 			break;
 		case(3):
-			UnosNaKraj(what, Head);
+			UnosKraj(UnosPodataka(), Head);
 			break;
 		case(4):
-			printf("Unesi prezime:\n");
+			printf("Unesite prezime:\t");
 			scanf("%s", prezime);
-			element= TraziElement(prezime, Head);
-			printf("%s %s %d", element->ime, element->prezime, element->godinaRodenja);
+			El = TraziElement(prezime, Head);
+			printf("%s %s %d", El->ime, El->prezime, El->godinaRodenja);
 			break;
 		case(5):
-			printf("Unesi prezime: ");
-			scanf(" %s", prezime);
+			printf("Unesite prezime:\t");
+			scanf("%s", prezime);
 			BrisiElement(TraziElement(prezime, Head), Head);
 			break;
-	}
+		case(0):
+			return 1;
+		}
+
+
+	} while (!status);
 
 	return 0;
 }
-Pozicija StvoriOsobu(char *ime, char *prezime, int *godinaRodenja)
+Osoba *UnosPodataka(void)
 {
-	Pozicija osoba = NULL;
-	osoba = (Pozicija)malloc(sizeof(Osoba));
+	Osoba *P = (Osoba*)malloc(sizeof(Osoba));
 
-	if (!osoba) {
-		printf("Greska!");
-		return NULL;
-	}
+	printf("Unesite ime, prezime i godinu rodenja: \n");
+	scanf("%s %s %d", P->ime, P->prezime, P->godinaRodenja);
 
-	strcpy(osoba->ime, ime);
-	strcpy(osoba->prezime, prezime);
-	strcpy(osoba->godinaRodenja, godinaRodenja);
-	osoba->next = NULL;
-
-	return osoba;
+	return P;
 }
-int UnosNaPocetak(Osoba *element, Pozicija P)
+int UnosPocetak(Osoba *El, Osoba *Head)
 {
-	element->next = P->next;
-	P->next = element;
+	El->next = Head->next;
+	Head->next = El;
 
 	return 0;
 }
-int IspisListe(Pozicija P)
+int IspisListe(Osoba *Head)
 {
+	Osoba *P = Head->next;
 	while (P != NULL) {
 		printf("%s %s %d", P->ime, P->prezime, P->godinaRodenja);
 		P = P->next;
 	}
-	return 0;
 }
-int UnosNaKraj(Osoba *element, Pozicija P)
+int UnosKraj(Osoba *El, Osoba *Head)
 {
-	while (P->next != NULL)
+	Osoba *P = Head;
+
+	while (P != NULL)
 		P = P->next;
 
-	element->next = NULL;
-	P->next = element;
+	El->next = NULL;
+	P->next = El;
 
 	return 0;
 }
-int TraziEelement(char *prezime, Pozicija P)
+Osoba *TraziElement(char *prezime, Osoba *Head)
 {
+	Osoba *P = Head->next;
+
 	while (P != NULL && strcmp(P->prezime, prezime))
 		P = P->next;
+
 	return P;
 }
-int BrisiElement(Osoba *element, Pozicija P)
+int BrisiElement(Osoba *El, Osoba *Head)
 {
-	
-	while (P != NULL && P->next != element)
+	Osoba *P = Head;
+
+	while (P != NULL && P->next != El)
 		P = P->next;
-	if (P == NULL)
-		printf("Element ne postoji.\n");
-	else {
-		P->next = P->next->next;
-		free(element);
-	}
+	
+	P->next = P->next->next;
+	free(El);
+
 	return 0;
 }
