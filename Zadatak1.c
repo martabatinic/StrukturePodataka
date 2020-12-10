ce715a3e62f23bad51495677e07695034161f460
martabatinic <56558228+martabatinic@users.noreply.github.com>
2020-12-10 12:25:31 +0100

    Update Zadatak1.c

--- Zadatak1.c
+++ Zadatak1.c
@@ -1,4 +1,4 @@
-/*Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata zapisanih u datoteci. 
+/*1. Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata zapisanih u datoteci. 
 Nakon toga potrebno je dinamički alocirati prostor za niz struktura studenata (ime, prezime, bodovi) i učitati iz datoteke sve zapise. 
 Na ekran ispisati ime, prezime,apsolutni i relativni broj bodova.
 Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
