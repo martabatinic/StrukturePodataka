e64264211b7eef92187ca366ee41d0adbb3a365c
martabatinic <56558228+martabatinic@users.noreply.github.com>
2020-12-11 16:51:32 +0100

    Update Zadatak1.c

--- Zadatak1.c
+++ Zadatak1.c
@@ -1,14 +1,14 @@
-/*1. Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata zapisanih u datoteci. 
-Nakon toga potrebno je dinamički alocirati prostor za niz struktura studenata (ime, prezime, bodovi) i učitati iz datoteke sve zapise. 
-Na ekran ispisati ime, prezime,apsolutni i relativni broj bodova.
-Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
-relatvan_br_bodova = br_bodova/max_br_bodova*100 */
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX 1000
+
+/*1. Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata zapisanih u datoteci. 
+Nakon toga potrebno je dinamički alocirati prostor za niz struktura studenata (ime, prezime, bodovi) i učitati iz datoteke sve zapise. 
+Na ekran ispisati ime, prezime,apsolutni i relativni broj bodova.
+Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+relatvan_br_bodova = br_bodova/max_br_bodova*100 */
 
 typedef struct student {
 	char ime[MAX];
