16c7bbd80b791ccc86804380d4829e8ea27b72b4
martabatinic <56558228+martabatinic@users.noreply.github.com>
2021-08-17 18:49:14 +0200

    Create Zadatak_11.c

---
+++ Zadatak_11.c
@@ -0,0 +1,197 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<malloc.h>
+
+#define MAX 256
+#define SIZE 11
+
+/*11. Napisati kod koji za zadane podatke studenata (matični broj, ime i prezime) pravi
+hash tablicu sa zasebnim redovima. Tablica ima 11 mjesta, a funkcija za preslikavanje
+ključ računa da se zbraja ASCII vrijednost prvih pet slova prezimena i zatim računa
+ostatak cjelobrojnog dijeljenja te vrijednosti s veličinom tablice. Studenti s istim
+ključem se pohranjuju u vezanu listu sortiranu po prezimenima i imenima.
+Kada se tablica izgradi treba ispisati cijelu tablicu (naglasiti na kojem ključu se nalaze
+koji podaci) te ponuditi mogućnost da se za određenog studenta (prezime i ime) ispiše
+njegov matični broj.*/
+
+typedef struct _student {
+	int maticniBroj;
+	char ime[MAX];
+	char prezime[MAX];
+	struct _student* next;
+}Student;
+
+Student** HashTable(int b);
+Student *NoviElement(void);
+int UcitajIzDatoteke(Student **Table, char *datoteka);
+int Unos(Student **Table, char *ime, char *prezime, int br);
+int IzracunTablice(char *prezime, int br);
+Student *Sortirano(Student *Head, char *ime, char *prezime);
+Student *Rewind(Student* Head, char* prezime);
+int IspisTablice(Student **Table, int b);
+int IspisListe(Student *Element, int i);
+int IspisMaticnogBroja(Student *Element);
+Student *PronadiElement(Student **Table, char *ime, char *prezime);
+
+int main(void)
+{
+	Student **Table;
+	char ime[MAX];
+	char prezime[MAX];
+	char datoteka[MAX];
+
+	Table = HashTable(SIZE);
+
+	printf("Kako se zove datoteka iz koje citate podatke? ");
+	scanf("%s", datoteka);
+	if (strchr(datoteka, '.') == NULL)
+		strcat(datoteka, ".txt");
+
+	UcitajIzDatoteke(Table, datoteka);
+
+	IspisTablice(Table, SIZE);
+
+	printf("Unesite ime i prezime za ispis Maticnog broja [IME PREZIME]: ");
+	scanf(" %s %s", ime, prezime);
+
+	IspisMaticnogBroja(PronadiElement(Table, ime, prezime));
+
+	return 0;
+}
+Student** HashTable(int a){
+	Student** Table = malloc(a*sizeof(Student*));
+
+	for (int i = 0; i < a; i++)
+		Table[i] = NoviElement();
+
+	return Table;
+}
+Student *NoviElement(void){
+	Student *Element = (Student*)malloc(sizeof(Student));
+
+	memset(Element->ime, 0, MAX);
+	memset(Element->prezime, 0, MAX);
+
+	Element->maticniBroj = 0;
+	Element->next = NULL;
+
+	return Element;
+}
+int UcitajIzDatoteke(Student **Table, char *datoteka) {
+	char ime[MAX];
+	char prezime[MAX];
+	int br;
+	char buffer[MAX];
+
+	FILE* fp = fopen(datoteka, "r");
+
+	if (fp == NULL)
+		printf("Neuspješno alociranje memorije!");
+
+	while (!feof(fp)) {
+		fgets(buffer, MAX, fp);
+		sscanf(buffer, "%s %s %d", ime, prezime, &br);
+		Unos(Table, ime, prezime, br);
+	}
+
+	fclose(fp);
+
+	return 0;
+}
+int Unos(Student **Table, char *ime, char *prezime, int br){
+	int pozicija = 0;
+	Student *Element = NULL;
+	Student *pom = NULL;
+
+	pozicija = IzracunTablice(prezime, SIZE);
+	Element = Sortirano(Table[pozicija], ime, prezime);
+
+
+	strcpy(Element->ime, ime);
+	strcpy(Element->prezime, prezime);
+	Element->maticniBroj = br;
+
+	return 0;
+}
+int IzracunTablice(char *prezime, int br){
+	int suma = 0;
+	
+	for (int i = 0; i < 5; i++)
+		suma += (int)prezime[i];
+
+	return suma % br;
+}
+Student *Sortirano(Student *Head, char *ime, char *prezime){
+	Student *P = Head;
+	Student *pom = NULL;
+
+	while (P->next && strcmp(prezime, P->next->prezime) > 0)
+		P = P->next;
+
+	if (P->next && !strcmp(prezime, P->next->prezime)) {
+		P = Rewind(Head, prezime);
+		while (P->next && !strcmp(prezime, P->next->prezime) && strcmp(ime, P->next->ime) > 0)
+			P = P->next;
+	}
+	pom = P->next;
+	P->next = NoviElement();
+	P = P->next;
+	P->next = pom;
+
+	return P;
+}
+Student *Rewind(Student* Head, char* prezime)
+{
+	Student *Element = prezime;
+
+	while (strcmp(Element->next->prezime, prezime))
+		Element = Element->next;
+
+	return Element;
+}
+int IspisTablice(Student **Table, int b){
+	for (int i = 0; i < b; i++)
+		if (Table[i]->next)
+			IspisListe(Table[i]->next, i);
+		else
+			printf("%d) --- \n", i);
+
+	return 0;
+}
+int IspisListe(Student *Element, int i){
+	Student *El = Element;
+
+	printf("\n%d)", i);
+
+	do {
+		printf(" %s %s\t", El->ime, El->prezime);
+		El = El->next;
+	} while (El);
+
+	return 0;
+}
+int IspisMaticnogBroja(Student *Element) {
+	if (Element == NULL)
+		printf("Unesen nepostojeci maticni broj!");
+
+	printf("%d\n", Element->maticniBroj);
+
+	return 0;
+}
+Student *PronadiElement(Student **Table, char *ime, char *prezime) {
+	int x;
+
+	x = IzracunTablice(prezime, SIZE);
+	Student *Element = Table[x]->next;
+
+	while (Element)
+		if (!strcmp(Element->prezime, prezime) && !strcmp(Element->ime, ime))
+			return Element;
+		else
+			Element = Element->next;
+
+	return Element;
+}
